9fefc2bd420888f66a189b64b100febfb4ba5de6
Marcus Jaensson <marcus@wahlstam.se>
2026-02-06 16:05:57 +0100

    Add shebang to install-standard-casks.sh

--- install-standard-casks.sh
+++ install-standard-casks.sh
@@ -1,1 +1,2 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -fsSL https://raw.githubusercontent.com/marcuswahlstam/ConfigMgr/refs/heads/main/standard-casks | brew bundle --file=-
