aa2f5ef835ee7716dc4a735b714239069a8a5d97
Marcus Jaensson <marcus@wahlstam.se>
2026-02-06 16:08:23 +0100

    Make Homebrew installation non-interactive

--- install-standard-casks.sh
+++ install-standard-casks.sh
@@ -1,2 +1,2 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -fsSL https://raw.githubusercontent.com/marcuswahlstam/ConfigMgr/refs/heads/main/standard-casks | brew bundle --file=-
