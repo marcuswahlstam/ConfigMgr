1e1ef4fcbd1ba532d6e11747cd391b30ff25a10d
Marcus Jaensson <marcus@wahlstam.se>
2026-02-06 16:14:33 +0100

    Enhance install-standard-casks.sh for user setup
    Add script to install Homebrew and standard casks for the current user.

--- install-standard-casks.sh
+++ install-standard-casks.sh
@@ -1,2 +1,9 @@
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+CURRENT_USER=$( scutil <<< "show State:/Users/ConsoleUser" | awk -F': ' '/[[:space:]]+Name[[:space:]]:/ { if ( $2 != "loginwindow" ) { print $2 }}' )
+
+echo >> /Users/$CURRENT_USER/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> /Users/$CURRENT_USER/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+
 curl -fsSL https://raw.githubusercontent.com/marcuswahlstam/ConfigMgr/refs/heads/main/standard-casks | brew bundle --file=-
